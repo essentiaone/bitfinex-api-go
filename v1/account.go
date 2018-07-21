66e6841a9867e8d7ca5c1beeb6c49db159de380f
Oxy Z <zaharchuk2ksenia@gmail.com>
2018-07-21 11:09:09 +0300

    Create method get fees for deposit and withdraw (#2)
        —  Create method get fees for deposit and withdraw

--- v1/account.go
+++ v1/account.go
@@ -1,5 +1,6 @@
 package bitfinex
 
+
 type AccountService struct {
 	client *Client
 }
@@ -34,62 +35,22 @@ func (a *AccountService) Info() (AccountInfo, error) {
 	return v[0], nil
 }
 
-type KeyPerm struct {
-	Read  bool
-	Write bool
+type Fees struct {
+	Withdraw  map[string]interface{} `json:"withdraw"`
 }
 
-type Permissions struct {
-	Account   KeyPerm
-	History   KeyPerm
-	Orders    KeyPerm
-	Positions KeyPerm
-	Funding   KeyPerm
-	Wallets   KeyPerm
-	Withdraw  KeyPerm
-}
-
-func (a *AccountService) KeyPermission() (Permissions, error) {
-	req, err := a.client.newAuthenticatedRequest("GET", "key_info", nil)
+// AccountFees return withdraw and deposit(are equal for 20.07.2018) fee
+func (a *AccountService) AccountFees() (Fees, error) {
+	req, err := a.client.newAuthenticatedRequest("POST", "account_fees", nil)
 
 	if err != nil {
-		return Permissions{}, err
+		return Fees{}, err
 	}
-
-	var v Permissions
-	_, err = a.client.do(req, &v)
+	var f Fees
+	_, err = a.client.do(req, &f)
 	if err != nil {
-		return Permissions{}, err
-	}
-	return v, nil
-}
-
-type SummaryVolume struct {
-	Currency string `json:"curr"`
-	Volume   string `json:"vol"`
-}
-type SummaryProfit struct {
-	Currency string `json:"curr"`
-	Volume   string `json:"amount"`
-}
-type Summary struct {
-	TradeVolume   SummaryVolume `json:"trade_vol_30d"`
-	FundingProfit SummaryProfit `json:"funding_profit_30d"`
-	MakerFee      string        `json:"maker_fee"`
-	TakerFee      string        `json:"taker_fee"`
-}
-
-func (a *AccountService) Summary() (Summary, error) {
-	req, err := a.client.newAuthenticatedRequest("GET", "summary", nil)
-
-	if err != nil {
-		return Summary{}, err
+		return Fees{}, err
 	}
 
-	var v Summary
-	_, err = a.client.do(req, &v)
-	if err != nil {
-		return Summary{}, err
-	}
-	return v, nil
+	return f, nil
 }
--- v1/account_test.go
+++ v1/account_test.go
@@ -46,38 +46,9 @@ func TestAccountInfo(t *testing.T) {
 	}
 }
 
-func TestAccountKeyPermission(t *testing.T) {
+func TestAccountFeesSuccess(t *testing.T) {
 	httpDo = func(req *http.Request) (*http.Response, error) {
-		msg := `{
-            "account":{
-                "read":true,
-                "write":false
-            },
-            "history":{
-                "read":true,
-                "write":false
-            },
-            "orders":{
-                "read":true,
-                "write":true
-            },
-            "positions":{
-                "read":true,
-                "write":true
-            },
-            "funding":{
-                "read":true,
-                "write":true
-            },
-            "wallets":{
-                "read":true,
-                "write":true
-            },
-            "withdraw":{
-                "read":null,
-                "write":null
-            }
-        }`
+		msg := `{"withdraw":{"BTC":"0.0004","LTC":"0.001","ETH":"0.0027","ETC":"0.01","ZEC":"0.001","XMR":"0.04","DSH":"0.01","XRP":"0.02","IOT":"0.5","EOS":0,"SAN":"1.2858","OMG":"0.17701","BCH":"0.0001","NEO":0,"ETP":"0.01","QTM":"0.01","AVT":"1.3861","EDO":"1.3211","BTG":0,"DAT":"18.126","QSH":"4.4763","YYW":"20.794","GNT":"4.0836","SNT":"17.915","BAT":"3.6035","MNA":"12.345","FUN":"46.949","ZRX":"1.0804","TNB":"52.735","SPK":"14.073","TRX":"34.395","RCN":"24.022","RLC":"1.6026","AID":"10.816","SNG":"27.003","REP":"0.042028","ELF":"2.0032","NEC":"3.5527","IOS":"55.82","AIO":"1.2415","REQ":"18.05","RDN":"1.5841","LRC":"2.939","WAX":"9.9654","DAI":"1.213","CFI":"33.789","AGI":"9.9694","BFT":"14.951","MTN":"15.528","ODE":"3.2154","ANT":"0.68002","DTH":"33.508","MIT":"1.9425","STJ":"2.3495","XLM":0,"XVG":0,"BCI":0,"MKR":"0.0020843","VEN":"0.61794","KNC":"1.3259","POA":"5.812","EVT":"480.99","LYM":"29.316","UTK":"13.484","VEE":"49.551","DAD":"10.046","ORS":"22.534","AUC":"7.5026","POY":"3.5383","FSN":"0.52736","CBT":"41.545","ZCN":"2.6471","SEN":"74.034","NCA":"74.197","CND":"31.769","CTX":"2.0613","PAI":0,"SEE":"180.0","ESS":"58.798","ATD":0,"ADD":0,"MTO":"12155.0","ATM":"36.053","HOT":"41.635","DTA":"33.755","IQX":0}}`
 
 		resp := http.Response{
 			Body:       ioutil.NopCloser(bytes.NewBufferString(msg)),
@@ -85,19 +56,47 @@ func TestAccountKeyPermission(t *testing.T) {
 		}
 		return &resp, nil
 	}
-	perm, err := NewClient().Account.KeyPermission()
+
+	info, err := NewClient().Account.AccountFees()
 
 	if err != nil {
-		t.Error(err)
+	t.Error(err)
 	}
 
-	if !perm.Account.Read {
-		t.Error("Expected", true)
-		t.Error("Actual ", perm.Account.Read)
+	if info.Withdraw["BTC"] != "0.0004" {
+	t.Error("Expected", "0.0004")
+	t.Error("Actual ", info.Withdraw["BTC"])
+	}
+}
+
+func TestAccountFeesFailed(t *testing.T) {
+	httpDo = func(req *http.Request) (*http.Response, error) {
+		msg := `{"response":
+					{"response":
+						{"request":
+							{"Status"  :"500 err",
+							"StatusCode": 500, 
+							"Method":"POST",
+							"URL":{"scheme": "http"} 
+							}
+						}
+					},	
+					"message":"some error"}`
+
+		resp := http.Response{
+			Body:       ioutil.NopCloser(bytes.NewBufferString(msg)),
+			StatusCode: 500,
+		}
+		return &resp, nil
 	}
 
-	if perm.History.Write {
-		t.Error("Expected", false)
-		t.Error("Actual ", perm.History.Write)
+	_, err := NewClient().Account.AccountFees()
+
+	if err == nil {
+		t.Error("TestAccountFeesFailed failed because of err = nil")
+		return
+	}
+	if err.Error() != "POST http://: 500 some error" {
+		t.Error("TestAccountFeesFailed failed because of err =", err)
 	}
 }
